1a3ee450df22d924cc6e50de85228b8891acf353
adithinaik281-glitch <adithinaik281@gmail.com>
2026-05-09 14:08:29 +0530

    Fix edit navigation using accession number fallback

--- admin/edit_book.php
+++ admin/edit_book.php
@@ -7,13 +7,19 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
 }
 
 $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
-if ($id <= 0) {
+$accessionNoFromQuery = isset($_GET['accession_no']) ? (int) $_GET['accession_no'] : 0;
+
+if ($id > 0) {
+    $stmt = $conn->prepare("SELECT * FROM books WHERE id = ?");
+    $stmt->bind_param("i", $id);
+} elseif ($accessionNoFromQuery > 0) {
+    $stmt = $conn->prepare("SELECT * FROM books WHERE accession_no = ?");
+    $stmt->bind_param("i", $accessionNoFromQuery);
+} else {
     header("Location: manage_books.php");
     exit();
 }
 
-$stmt = $conn->prepare("SELECT * FROM books WHERE id = ?");
-$stmt->bind_param("i", $id);
 $stmt->execute();
 $result = $stmt->get_result();
 if ($result->num_rows !== 1) {
@@ -21,6 +27,7 @@ if ($result->num_rows !== 1) {
     exit();
 }
 $book = $result->fetch_assoc();
+$id = (int) $book['id'];
 $error = "";
 
 if (isset($_POST['update'])) {
@@ -47,7 +54,7 @@ if (isset($_POST['update'])) {
         $error = "Accession number already used by another book.";
     } else {
         $update = $conn->prepare("UPDATE books SET date_of_accession=?, accession_no=?, category=?, author=?, title=?, publisher=?, year=?, price=?, total_copies=?, quantity=?, bill_no=?, bill_date=?, supplier=?, edition=?, remarks=? WHERE id=?");
-        $update->bind_param("sisssssdiiissssi", $dateOfAccession, $accessionNo, $subject, $author, $title, $publisher, $year, $price, $total, $quantity, $billNo, $billDate, $supplier, $edition, $remarks, $id);
+        $update->bind_param("sisssssdiisssssi", $dateOfAccession, $accessionNo, $subject, $author, $title, $publisher, $year, $price, $total, $quantity, $billNo, $billDate, $supplier, $edition, $remarks, $id);
         $update->execute();
         header("Location: manage_books.php");
         exit();
--- admin/manage_books.php
+++ admin/manage_books.php
@@ -93,7 +93,7 @@ if ($search !== '') {
                                     <td>{$row['quantity']}</td>
                                     <td>{$row['date_of_accession']}</td>
                                     <td>
-                                        <a href='edit_book.php?id={$row['id']}' class='badge-btn badge-edit'>Edit</a>
+                                        <a href='edit_book.php?accession_no={$row['accession_no']}' class='badge-btn badge-edit'>Edit</a>
                                         <a href='manage_books.php?delete={$row['id']}' class='badge-btn badge-delete' onclick=\"return confirm('Are you sure you want to delete this book?')\">Delete</a>
                                     </td>
                                 </tr>";
